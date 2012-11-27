b58ef6300c5899822c10970021d878f96f073c4a
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2012-11-27 09:18:20 +0530

    ready to proceed by khushbu

---
+++ page/storedata.php
@@ -0,0 +1,11 @@
+<?php
+
+class page_storedata extends Page {
+	function init(){
+		parent::init();
+
+		$tabs=$this->add('Tabs');
+		$tabs->addTabURL('store_iteminward','Item Inward');
+		$tabs->addTabURL('store_itemoutward','Item Outward');
+	}
+}
