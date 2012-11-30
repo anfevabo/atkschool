3711b23daed687b21d99c1c3440e9c399df3115d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2012-11-30 15:23:28 +0530

    corrections correcetd

--- lib/Model/Students/Movement.php
+++ lib/Model/Students/Movement.php
@@ -9,6 +9,8 @@ class Model_Students_Movement extends Model_Table{
 		$this->addField('date')->type('datetime')->defaultValue(date('Y-m-d H:i:s'));
 		$this->addField('purpose')->enum(array('inward','outward','enquiry'))->mandatory('Purpose must be selected');
 		$this->addField('remark');
+		$this->addField('direction');
+
 		$this->_dsql()->order('id','desc');
 	}
 }
