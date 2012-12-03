a8ac3a77535aac0f3eab9e73e610006446bbd40e
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2012-12-03 12:28:06 +0530

    receipt complete

--- page/schooldata.php
+++ page/schooldata.php
@@ -5,8 +5,8 @@ class page_schooldata extends Page {
 		parent::init();
 		$acl=$this->add('xavoc_acl/Acl');
 		$tabs=$this->add('Tabs');
+		$tabs->addtabURL('student_rollnoallotment','Roll Numbers');
 		$tabs->addtabURL('school_scholars','Sessions Students');
 		$tabs->addtabURL('school_studentClassMapping','Student Class Association');
-		$tabs->addtabURL('student_rollnoallotment','Roll Numbers');
 	}
 }
--- page/student/rollnoallotment.php
+++ page/student/rollnoallotment.php
@@ -1,9 +1,56 @@
 <?php
 class page_student_rollnoallotment extends Page{
 	function page_index(){
-		$form=$this->add('Form',null,null,array('form_horizonatal'));
-		$class_field=$this->addField('dropdown','class');
+
+		$form=$this->add('Form',null,null,array('form_horizontal'));
+		$class_field=$form->addField('dropdown','class')->setEmptyText('-----');
+		$class_field->setModel('Class');
+		$class_field->setAttr('class','hindi');
+		$roll_field=$form->addField('line','roll_no')->setNotNull();
+
+		$form->addSubmit("Allot");
+
+		$c=$this->add('Model_Students_Current');
+		$grid=$this->add('Grid');
+		if($_GET['class']){
+			$c->addCondition('class_id',$_GET['class']);
+		}else{
+			$c->addCondition('class_id',-1);
+		}
+
+		$grid->setModel($c, array('name','class','roll_no'));
+		$grid->addColumn('Expander','edit','Edit');
+		
+		$grid->addClass('reladable_grid');
+		$grid->js('reloadme',$grid->js()->reload());
+
+		if($form->isSubmitted()){
+
+			$students=$this->add('Model_Students_Current');
+			$students->addCondition('class_id',$form->get('class'));
+			$start_roll_no=$form->get('roll_no');
+			foreach ($students as $junk) {
+				$students['roll_no'] = $start_roll_no ++;
+				$students->save();
+			}
+			$grid->js(null,$form->js()->reload())->reload(array("class"=>$form->get("class")))->execute();
+		}
+
+		$class_field->js('change',$grid->js()->reload(array('class'=>$class_field->js()->val())));
 
 
+	}
+
+	function page_edit(){
+		$this->api->stickyGET('student_id');
+		$m=$this->add('Model_Students_Current');
+		$m->load($_GET['student_id']);
+		$form = $this->add('Form');
+		$form->setModel($m,array('roll_no'));
+		if($form->isSubmitted()){
+			$form->update();
+			$form->js()->univ()->successMessage('Upadetd')->closeExpander()->execute();
+			// $form->js()->_selector('.reladable_grid')->reload(array('class'=>$m['class_id']))->execute();
+		}
 	}
 }
