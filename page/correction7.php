3dc82a0c24570907048c09f4fa8e5e2efaafae00
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2013-03-23 11:03:27 +0530

    Merged Marksheet work from xserver

--- page/correction7.php
+++ page/correction7.php
@@ -3,8 +3,8 @@ class page_correction7 extends Page{
 	function init(){
 		parent::init();
 		$this->query("UPDATE `fee_applicable` SET  `due`=`amount`;");	
-		$this->query("ALTER TABLE `marksheet_sections` ADD `extra_totals` VARCHAR( 255 ) NOT NULL");	
-		$this->query("ALTER TABLE `marksheet_sections` ADD `column_code` VARCHAR( 255 ) NOT NULL ");	
+		// $this->query("ALTER TABLE `marksheet_sections` ADD `extra_totals` VARCHAR( 255 ) NOT NULL");	
+		// $this->query("ALTER TABLE `marksheet_sections` ADD `column_code` VARCHAR( 255 ) NOT NULL ");	
 		
 		}
 	function query($q) {
